dbad4ec141720a333cc2b5732e3e82ae542d125b
tazzaoui <tazzaoui617@gmail.com>
2017-12-29 12:06:04 -0500

    Defined residual computation

--- univariate.py
+++ univariate.py
@@ -1,1 +1,9 @@
 import matplotlib
+
+def compute_residual(coefs, data):
+    squared_residual = 0
+    for i in range(0,len(data)):
+        hypothesis = coefs[0] + coefs[1] * data[i,0]
+        squared_residual += (hypothesis - data[i,1]) ** 2
+    residual = squared_residual / (2*len(data))
+    return residual
