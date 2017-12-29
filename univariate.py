e7020e4f46e51f685e68a355cde0b39fa7a50c3c
tazzaoui <tazzaoui617@gmail.com>
2017-12-29 14:52:01 -0500

    Added parameter update

--- univariate.py
+++ univariate.py
@@ -10,3 +10,16 @@ def compute_residual(coefs, data):
         squared_residual += (hypothesis - data[i,1]) ** 2
     residual = squared_residual / (2*len(data))
     return residual
+
+
+# Update parameters according to the partials of the cost function
+def update_coefs(coefs, data, alpha):
+    hb = hm = 0
+    for i in range(0, len(data)):
+        hypothesis = (coefs[0] + coefs[1] * data[i,0]) - data[i,1]
+        hb += hypothesis
+        hm += hypothesis*data[i,0]
+    hb *= alpha/len(data)
+    hm *= alpha/len(data)
+    return hb, hm
+
