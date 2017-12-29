776ae9453b4ab4cbd468dd9630eb5d2dcffc194b
tazzaoui <tazzaoui617@gmail.com>
2017-12-29 14:53:29 -0500

    Implemented scatterplot of data

--- univariate.py
+++ univariate.py
@@ -11,8 +11,7 @@ def compute_residual(coefs, data):
     residual = squared_residual / (2*len(data))
     return residual
 
-
-# Update parameters according to the partials of the cost function
+# Update each coef according to the partial deriv. of the cost function
 def update_coefs(coefs, data, alpha):
     hb = hm = 0
     for i in range(0, len(data)):
@@ -23,3 +22,10 @@ def update_coefs(coefs, data, alpha):
     hm *= alpha/len(data)
     return hb, hm
 
+def plot_data(data, coefs,i):
+    pts, line = plt.subplots()
+    l, = line.plot(data[:,0], coefs[1] * data[:,0] + coefs[0], color='red')
+    plt.scatter(data[:,0], data[:,1])
+    plt.title("Hypothesis: Y = {}x + {}".format(coefs[1], coefs[0]))
+    plt.show()
+    
